b096e0132e9094ac0ccb9d89689263c2ddb36c13
HOSPITAL - Sistemas01 <sistemas01@hospital.local>
2021-09-22 16:13:54 -0500

    validacion edad

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,45 +1,6 @@
-//valido la edad. tiene que ser entero mayor que 18
-var edad = document.getElementById("dato_edad_usuario").value;
-//var edad = document.form_nuevo_usuario.dato_edad_usuario.value;
-console.log(edad);
-/*edad = validarEntero(edad)
-document.form_nuevo_usuario.dato_edad_usuario.value=edad
-    if (edad==""){
-        alert("Tiene que introducir un número entero en su edad.")
-        document.form_nuevo_usuario.dato_edad_usuario.focus()
-        return 0;
-        }
-        else{
-            if (edad<18){
-                alert("Debe ser mayor de 18 años.")
-                document.form_nuevo_usuario.dato_edad_usuario.focus()
-                return 0;
-                }
-        }*/
-
-function validarEntero(valor) {
-    valor = parseInt(valor);
-    if (isNaN(valor)) {
-        console.log("Campo vacio o no numerico");
-        return "";
-    }
-        else {
-            console.log("Campo numerico", +valor);
-            return valor;
-        }
-}
-
 function validaEdad() {
-    enteroValidado = validarEntero(edad)
-    if (enteroValidado == ""){
-        alert("Debe Escribir un numero entero");
-        document.form_nuevo_usuario.dato_edad_usuario.clear;
-        document.form_nuevo_usuario.dato_edad_usuario.select();
-        //document.form_nuevo_usuario.dato_edad_usuario.focus();
-    }
-        else {
-            document.form_nuevo_usuario.dato_edad_usuario.value = enteroValidado
-        }
+    var edad = document.getElementById("dato_edad_usuario").value;
+    var enteroValidado = validar_edad_usuario(edad);
 }
 
 function validar_nombre_usuario(string){
@@ -47,6 +8,16 @@ function validar_nombre_usuario(string){
 }
 
 function validar_edad_usuario(edad){
+    var valor = parseInt(edad);
+    console.log(valor);
+    if(isNaN(valor)== true){
+        console.log(valor);
+        alert("Por el if -->> No se puede convertir");
+    }
+    else{
+        console.log(valor);
+        alert("Por el else")
+    }
 
 }
 
