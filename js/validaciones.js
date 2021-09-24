26d525d6e42f97895aeec21150f571786f73ce68
guivalenator <guivalenator@gmail.com>
2021-09-23 22:13:37 -0500

    valiadaciones Luis V

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,58 +1,62 @@
-//valido la edad. tiene que ser entero mayor que 18
-var edad = document.getElementById("dato_edad_usuario").value;
-//var edad = document.form_nuevo_usuario.dato_edad_usuario.value;
-console.log(edad);
-/*edad = validarEntero(edad)
-document.form_nuevo_usuario.dato_edad_usuario.value=edad
-    if (edad==""){
-        alert("Tiene que introducir un número entero en su edad.")
-        document.form_nuevo_usuario.dato_edad_usuario.focus()
-        return 0;
-        }
-        else{
-            if (edad<18){
-                alert("Debe ser mayor de 18 años.")
-                document.form_nuevo_usuario.dato_edad_usuario.focus()
-                return 0;
-                }
-        }*/
-
-function validarEntero(valor) {
-    valor = parseInt(valor);
-    if (isNaN(valor)) {
-        console.log("Campo vacio o no numerico");
-        return "";
-    }
-        else {
-            console.log("Campo numerico", +valor);
-            return valor;
-        }
-}
-
 function validaEdad() {
-    enteroValidado = validarEntero(edad)
-    if (enteroValidado == ""){
-        alert("Debe Escribir un numero entero");
-        document.form_nuevo_usuario.dato_edad_usuario.clear;
-        document.form_nuevo_usuario.dato_edad_usuario.select();
-        //document.form_nuevo_usuario.dato_edad_usuario.focus();
-    }
-        else {
-            document.form_nuevo_usuario.dato_edad_usuario.value = enteroValidado
-        }
+    var edad = document.getElementById("dato_edad_usuario").value;
+    var enteroValidado = validar_edad_usuario(edad);
 }
 
-function validar_nombre_usuario(string){
+function validaNombre() {
+    var nombre = document.getElementById("dato_nombre_usuario").value;
+    // nombreValidado = validar_nombre_usuario(nombre);
+    var nombreValidado = validar_nombre_usuario(nombre);
+}
+
+function validar_nombre_usuario(valor){
+    console.log(valor);
+    var dato_a_comprobar = valor;
+    var valoresNoAceptados = /^[0-9\s]+$/;
+    var valoresAceptados = /^[a-zA-Z0-9\s]+$/;
+    var numero_caracteres= valor.length;
+    console.log(valor[valor.length-1])
+    console.log(numero_caracteres);
+    if (valor[0].match(valoresNoAceptados) || valor[valor.length-1]==" ") {
+        alert("Nombre de usuario no debe iniciar con un número")
+        return false;
+    }
+    else if(dato_a_comprobar.match(valoresAceptados) && dato_a_comprobar[0].match(/^[A-Z]+$/ )){
+        for(var i=0; dato_a_comprobar.length-1;i++){
+            if(dato_a_comprobar[i]==" "){
+                console.log("Encontro un espacio")
+                console.log(dato_a_comprobar[i+1]);
+                if (dato_a_comprobar[i+1].match(/^[A-Z]+$/));
+                    alert("Correcto")
+            }
+        } 
+    }
+    else{
+        alert("Las letras iniciales deben ser en mayusculas")
+    }
 
 }
 
 function validar_edad_usuario(edad){
+    var valor = parseInt(edad);
+    console.log(valor);
+    if(isNaN(valor)== true){
+        console.log(valor);
+        alert("El dato ingresado debe ser numerico");
+    }
+    else if(valor >=13 && valor <110){
+        console.log(valor);
+        alert("Cumple con los parametros de edad")
+    }
+    else{
+        alert("La edad  debe estar entre 13 y 109 años")
+    }
 
 }
 
 function validar_contasena(string){
 
 }
-module.exports.validar_nombre_usuario = validar_nombre_usuario;
-module.exports.validar_edad_usuario = validar_edad_usuario;
-module.exports.validar_contasena = validar_contasena;
+//module.exports.validar_nombre_usuario = validar_nombre_usuario;
+//module.exports.validar_edad_usuario = validar_edad_usuario;
+//module.exports.validar_contasena = validar_contasena;
